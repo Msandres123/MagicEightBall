99b0f77469f96764ce85f2ae8e79907177dbb75d
Mikael Andres <msandres123@gmail.com>
2022-02-03 11:42:44 -0500

    complete

--- eightball.py
+++ eightball.py
@@ -5,9 +5,8 @@ question = "Will I make good money tonight at work??"
 
 answer = ""
 
-random_number = random.randint(1,9)
+random_number = random.randint(1,11)
 
-# print(random_number)
 if random_number == 1: 
   answer = "Yes - definitely."
 elif random_number == 2:
@@ -26,7 +25,17 @@ elif random_number == 8:
   answer = "Outlook not so good"
 elif random_number == 9:
   answer = "Very doubtful."
+elif random_number == 10:
+  answer = "Fasho Fasho."
+elif random_number == 11:
+  answer = "Hell to the nah bruh."
 else:
   answer = "Error"
-print(name +" asks: "+ question)
-print (answer)
+if question == "": 
+  print("The Magic 8-Ball needs a question in order to provide an answer")
+else: 
+  if name == "": 
+    print("Question: "+ question)
+  else:
+    print(name +" asks: "+ question)
+    print ("Magic 8-Ball's answer: " + answer)
